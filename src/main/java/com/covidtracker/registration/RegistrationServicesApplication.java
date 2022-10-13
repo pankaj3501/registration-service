7689e780b1fbe4b63b847acd88565ccb27369aad
pankaj3501 <mr.pankajkumar3501@gmail.com>
2022-10-14 00:01:47 +0530

    first commit

---
+++ src/main/java/com/covidtracker/registration/RegistrationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.covidtracker.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covidtracker/registration/RegistrationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidtracker.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
